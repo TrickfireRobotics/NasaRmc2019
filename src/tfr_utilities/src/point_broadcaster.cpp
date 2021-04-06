f874d75a75b31427e98496b11f3aeb5a6fa66b02
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-04-05 18:17:03 -0700

    updating so odom will always be at center of robot

--- src/tfr_utilities/src/point_broadcaster.cpp
+++ src/tfr_utilities/src/point_broadcaster.cpp
@@ -86,10 +86,10 @@ int main(int argc, char** argv)
 
     //broadcast the point across the network
     ros::Rate rate(hz);
-    while(ros::ok())
-    {
+    //while(ros::ok())
+    //{
         broadcaster.broadcast();
         ros::spinOnce();
-        rate.sleep();
-    }
+        //rate.sleep();
+    //}
 }
