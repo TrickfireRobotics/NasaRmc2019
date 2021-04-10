71c30e1ea1f68124202cc84ea9c4316dfef0c354
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-04-09 17:43:52 -0700

    Update monitoring_battery.cpp

--- src/tfr_monitoring/src/monitoring_battery.cpp
+++ src/tfr_monitoring/src/monitoring_battery.cpp
@@ -14,9 +14,7 @@
 
 void batteryVoltageCallback(const std_msgs::UInt16& batteryVoltage) {
 	//Roboteq controller sends voltage * 10 back i.e. if 150 is reported than it is 15 volts.
-	//What is reported is diveded by 10 to get the actual voltage.
         int batteryVolt = batteryVoltage.data / 10;
-        //if battery voltage is below 37 than it needs to be charged
         if (batteryVolt < 37) {
         	ROS_ERROR("BATTERY LOW! CHARGE NOW! (%d volts)\n", batteryVolt);
         }
